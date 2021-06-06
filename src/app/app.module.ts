934b81264e797706df0c42e2d00c6da37f492262
victorHugo <victorhugoaguilar@aol.com>
2021-06-06 11:29:20 +0200

    add module for auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFoundComponent } from './pages/nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NoPageFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     PagesModule
   ],
   bootstrap: [AppComponent]
