4044957f1f0ef1f316cb2ffe8c2abc353c828619
victorHugo <victorhugoaguilar@aol.com>
2021-06-06 14:45:11 +0200

    add component child for graphics

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,18 +3,23 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IncrementsComponent } from './increments/increments.component';
+import { DonaComponent } from './dona/dona.component';
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [
-    IncrementsComponent
+    IncrementsComponent,
+    DonaComponent
   ],
   imports: [
     CommonModule,
+    ChartsModule,
     FormsModule
   ],
   exports: [
-    IncrementsComponent
+    IncrementsComponent,
+    DonaComponent
   ]
 })
 export class ComponentsModule { }
