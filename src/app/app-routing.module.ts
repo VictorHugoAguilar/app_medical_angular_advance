fdd9b1b72d0f74d51b3f2ba1803e7dc9f9e0fffa
victorHugo <victorhugoaguilar@aol.com>
2021-06-06 11:45:53 +0200

    change routing for login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Modules
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 import { NoPageFoundComponent } from './errors/404/nopagefound.component';
 
 const routes: Routes = [
-
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-
   { path: '**', component: NoPageFoundComponent },
-
 ]
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
