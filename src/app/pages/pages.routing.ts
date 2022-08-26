c118c4b9257503eae79237eb9571445efabd3889
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-08-26 02:21:40 +0200

    add routing pages medicos and hospitales

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -12,6 +12,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
 import { AuthGuard } from '../guards/auth.guard';
 import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
+import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
 
 const routes: Routes = [
   {
@@ -29,6 +31,8 @@ const routes: Routes = [
 
       // Mantenimientos
       { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Usuarios de aplicación' } },
+      { path: 'medicos', component: MedicosComponent, data: { titulo: 'Medicos de aplicación' } },
+      { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Hospitales de aplicación' } },
     ]
   },
 ];
