098f22b8979d91015e59542997c831c7de4a67cd
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-08-28 22:39:24 +0200

    add minimal changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,6 @@ export const environment = {
   production: true,
   // ruta donde se ha desplegado
   base_url: 'https://admin-hospital-backend-test.herokuapp.com/',
+  CLIENT_ID:"597845449180-mf4cqurem6pkpigfidv8u9ario80ld4b.apps.googleusercontent.com",
+  SECRET_CLIENT:'9n1Q7lc5Ayaq2pPFSXXlGtl4'
 };
