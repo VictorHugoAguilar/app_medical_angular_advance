4bf7d16950b1c088479445b9724997960faaeec1
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-08-28 13:14:14 +0200

    comment

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -11,6 +11,12 @@ export class AdminGuard implements CanActivate {
   constructor(private usaurioService: UsuarioService,
     private router: Router) { }
 
+  /**
+   * Check if user contain el role ADMIN_ROLE
+   * @param route 
+   * @param state 
+   * @returns false o true
+   */
   canActivate(route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
     if (this.usaurioService.role === 'ADMIN_ROLE') {
